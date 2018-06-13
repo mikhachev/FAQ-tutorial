9f20f3c3c82a7c336a8237e668e8f809f4d20bbb
mikhachev <36811961+mikhachev@users.noreply.github.com>
2018-06-13 20:42:25 +0300

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,17 +11,15 @@ class UsersTableSeeder extends Seeder
      * @return void
      */
     public function run()
-    {  
-       //$date = date("Y-m-d H:i:s");
-       //$new_date = date('d.m.Y H:i:s', strtotime("+3 hours", strtotime($date)));
-       DB::table('users')->insert([
+    {
+        DB::table('users')->insert([
             'name' => 'admin2',
             'email' => 'admin2@admin.com',
             'password' => bcrypt('admin'),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
-           // 'created_at' =>  date("Y-m-d H:i:s"),
-           // 'updated_at' =>  date("Y-m-d H:i:s"),
+            // 'created_at' =>  date("Y-m-d H:i:s"),
+            // 'updated_at' =>  date("Y-m-d H:i:s"),
         ]);
     }
 }
